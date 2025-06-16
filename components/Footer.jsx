1c5421ebc5fb443a4a7fa3cc3adc6c79d479a95f
ภูริพัฒน์ ศักดิ์นุกูล <phuriphat1793@hotmail.com>
2025-06-17 00:32:01 +0700

    Refactor code structure for improved readability and maintainability

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { assets } from "@/assets/assets";
 import Image from "next/image";
 
 const Footer = () => {
@@ -7,7 +6,13 @@ const Footer = () => {
     <footer>
       <div className="flex flex-col md:flex-row items-start justify-center px-6 md:px-16 lg:px-32 gap-10 py-14 border-b border-gray-500/30 text-gray-500">
         <div className="w-4/5">
-          <Image className="w-28 md:w-32" src={assets.logo} alt="logo" />
+          <Image
+            className="w-28 md:w-32"
+            src="/DekOuan.svg"
+            alt="logo"
+            width={128}
+            height={40}
+          />
           <p className="mt-6 text-sm">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
@@ -21,16 +26,24 @@ const Footer = () => {
             <h2 className="font-medium text-gray-900 mb-5">Company</h2>
             <ul className="text-sm space-y-2">
               <li>
-                <a className="hover:underline transition" href="#">Home</a>
+                <a className="hover:underline transition" href="#">
+                  Home
+                </a>
               </li>
               <li>
-                <a className="hover:underline transition" href="#">About us</a>
+                <a className="hover:underline transition" href="#">
+                  About us
+                </a>
               </li>
               <li>
-                <a className="hover:underline transition" href="#">Contact us</a>
+                <a className="hover:underline transition" href="#">
+                  Contact us
+                </a>
               </li>
               <li>
-                <a className="hover:underline transition" href="#">Privacy policy</a>
+                <a className="hover:underline transition" href="#">
+                  Privacy policy
+                </a>
               </li>
             </ul>
           </div>
